faab72dedad48d848b661fd1ba7e9fcfacddfb92
Gabs00 <gewen87@gmail.com>
2014-07-04 02:24:04 -0400

    created a function for dynamically create html elements; created update stream function that updates the timeline and limits it to 10 posts

--- twit.js
+++ twit.js
@@ -1,15 +1,47 @@
 $(document).ready(function(){
-    var $body = $('body');
+   /* var $body = $('body');
     $body.html('');
 
     var index = streams.home.length - 1;
-    console.log(index);
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<div></div>');
       $tweet.text('@' + tweet.user + ': ' + tweet.message);
       $tweet.appendTo($body);
       index -= 1;
+    }
+    */
+    update_stream();
+    function update_stream(){
+        var index = streams.home.length-1;
+        while(index >=0){
+          var tweet = streams.home[index];
+          var text = '@' + tweet.user + ': ' + tweet.message;
+          var div = create_div('<div></div>', text, 'messages');
+          var history = $('#history').children();
+          if($(history).length >= 10){
+             $('history').last().remove();
+          }
+          $(div).prependTo('#history');
+          index-=1;
+        }
+    }
+
+    function create_div(type, text, item_class, item_id){
+      if(!type){
+        return undefined;
+      }
+      var new_div = $(type);
+      if(item_class){
+        $(new_div).prop('class', item_class);
+      }
+      if(item_id){
+        $(new_div).prop('id', item_id)
+      }
+      if(text){
+        $(new_div).text(text);
+      }
+      return new_div;
     }
 
 });
