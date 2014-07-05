418f34552c64912d5511a3d730503ab92014e8a6
Gabs00 <gewen87@gmail.com>
2014-07-04 23:26:43 -0400

    completed the add function of the container method. Can add and remove new tweets to the bottom array for usage later, updates the history list with new tweets

--- Timeline.js
+++ Timeline.js
@@ -21,26 +21,20 @@ function Timeline(item_id, arrays, item_style){
 Timeline.prototype = new Object();
 Timeline.prototype.constructor = new Timeline();
 Timeline.prototype.Update = function(){ 
-    var self = this;
-    this.displayed.forEach(
-        function(value, index){
-            var list_id = "h_" + index;
-            if(value[1]){
-                if(value[0] !== $(value[1]).html()){
-                    
-                     $(list_id).val(value[0]);
-                    value.push($(list_id));
-                }
-            }
-            else{
-                var new_item = $('<li>'+value[0]+'</li>');
-                $(new_item).attr('id', list_id);
-                console.log($(new_item).html())
-                $(new_item).prependTo($(self.list));
-                value.push($(new_item));
-                
-            }
-        })
+    $(this.list).empty();
+    var len = this.displayed.length-1;
+    for(var i = len; i >= 0; i--){
+            
+            var value = this.displayed[i];
+            var list_id = "h_" + i;
+            $(value[1]).remove();
+            
+            var new_item = $('<li>'+value[0]+'</li>');
+            $(new_item).attr('id', list_id);
+            $(new_item).prependTo($(this.list));
+            
+            value[1]=$(new_item);
+    }
 }
 
 function timeline_container(item_id, outer_arrays,item_style){
@@ -49,25 +43,18 @@ function timeline_container(item_id, outer_arrays,item_style){
         if(!item){
              return undefined;   
         }
+
+        tl.displayed.unshift([item]);
+        
         if(tl.length >= 10){
             tl.bottom.unshift(tl.displayed.pop().shift());
-        }
+        }       
         
-        tl.displayed.unshift([item]);
         tl.length++;
         tl.Update();
     }
-    function show_display(){
-        return tl.displayed.toString() || 0;   
-    }
+
     return {
         add: add_new,
-        display: show_display,
     };
 }
-
-
-var l = timeline_container('#history', undefined, '.history');
-l.add('hello');
-l.add('world');
-l.add('!')
