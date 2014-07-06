0aeb3214fcd0dda4193b40132a578a9bc3aede5a
Gabs00 <gewen87@gmail.com>
2014-07-05 23:02:55 -0400

    Added constants max_display, max_bottom, max_top to get rid of magic numbers in timeline_container

--- scripts/Timeline.js
+++ scripts/Timeline.js
@@ -20,20 +20,27 @@ function Timeline(item_id, arrays, item_style){
 
 Timeline.prototype = new Object();
 Timeline.prototype.constructor = new Timeline();
+Timeline.prototype.limits = {
+        max_display: [10,22],
+        max_bottom: 100,
+        max_top: 100,
+};
+
 Timeline.prototype.Update = function(){ 
     $(this.list).empty();
     var len = this.displayed.length-1;
     for(var i = len; i >= 0; i--){
             
-            var value = this.displayed[i];
-            var list_id = "h_" + i;
-            $(value[1]).remove();
+        var value = this.displayed[i];
+        var list_id = "h_" + i;
+        
+        $(value[1]).remove();
             
-            var new_item = $('<li>'+value[0]+'</li>');
-            $(new_item).attr('id', list_id);
-            $(new_item).prependTo($(this.list));
+        var new_item = $('<li>'+value[0]+'</li>');
+        $(new_item).attr('id', list_id);
+        $(new_item).prependTo($(this.list));
             
-            value[1]=$(new_item);
+        value[1]=$(new_item);
     }
 }
 
@@ -77,7 +84,7 @@ function timeline_container(item_id, outer_arrays,item_style){
     
     function less(amount){
         var max = tl.displayed.length - 10;
-        
+
         if(amount === undefined){
              amount = max;
         }
